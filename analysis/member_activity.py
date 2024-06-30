e645b0d1ace95a985ffca94ee41302d544952c90
b5h22 <byoo3@myseneca.ca>
2024-06-29 23:33:34 -0400

    edit: entry trend plot titles for all facilities

--- analysis/member_activity.py
+++ analysis/member_activity.py
@@ -8,7 +8,7 @@ def plot_entry_trends_day(entry_trends_day):
     norm = mcolors.Normalize(vmin=0, vmax=entry_trends_day.max())
     plt.figure(figsize=(10, 6))
     ax = entry_trends_day.plot(kind='bar', color=[cmap(norm(value)) for value in entry_trends_day])
-    ax.set_title('Entry Trends by Day of the Week')
+    ax.set_title('All Facilities Entry Trends by Day of the Week')
     ax.set_xlabel('Day of the Week')
     ax.set_ylabel('Number of Entries')
     for p in ax.patches:
@@ -20,7 +20,7 @@ def plot_entry_trends_time(entry_trends_time):
     norm = mcolors.Normalize(vmin=0, vmax=entry_trends_time.max())
     plt.figure(figsize=(10, 6))
     ax = entry_trends_time.plot(kind='bar', color=[cmap(norm(value)) for value in entry_trends_time])
-    ax.set_title('Entry Trends by Time of Day')
+    ax.set_title('All Facilities Entry Trends by Time of Day')
     ax.set_xlabel('Hour of Day')
     ax.set_ylabel('Number of Entries')
     for p in ax.patches:
